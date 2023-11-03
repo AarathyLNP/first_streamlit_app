8346143dd1bceddd485471d324b1cd165d747be2
AarathyLNP <148954271+AarathyLNP@users.noreply.github.com>
2023-11-03 15:32:33 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,9 +20,11 @@ streamlit.dataframe(fruits_to_show)
 #New Section to display fruityvice api response
 
 import requests
-streamlit.header('Fruityvice Fruit Advice!')
-fruit_choice = streamlit.text_input('What fruit would you like information about?', 'Kiwi')
-streamlit.write('The user entered', fruit_choice)
-fruityvice_ response = requests.get("https://fruityvice.com/api/fruit/" + 'kiwi')
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+streamlit.text(fruityvice response.ison) # just writes the data to the screen
+# take the ison version of the response and normalize it
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json ())
+#output it the screen as a table
+streamlit.dataframe(fruityvice_normalized)
 
 import snowflake.connector
